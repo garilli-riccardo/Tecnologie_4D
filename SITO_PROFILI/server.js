02c2510e34224a6c40a8b13a1304bd20b7f66307
Riccardo Garilli <garilli.riccardo@iisgalvanimi.edu.it>
2024-03-11 06:00:06 +0000

    seconda parte finita, con qualche errore

--- SITO_PROFILI/server.js
+++ SITO_PROFILI/server.js
@@ -5,6 +5,15 @@ const port = 3000;
 app.use(express.static('public')); // Per servire file statici come immagini e CSS
 app.set('view engine', 'pug'); // Usa Pug come motore di template
 
+function getProfileDataAbel(nome) {
+  return {
+    nome: nome,
+    eta: '30',
+    biografia: 'Una breve biografia del profilo...',
+    genere_musicale: '',
+  };
+}
+
 // Homepage
 app.get('/', (req, res) => {
   res.render('index', { title: 'Sito Profili' });
@@ -19,6 +28,13 @@ app.get('/profili', (req, res) => {
 app.get('/profili/:persona', (req, res) => {
   const persona = req.params.persona;
   res.render('persona', { title: `Profilo di ${persona}`, persona });
+});
+
+app.get('/profilo/:nome', (req, res) => {
+  const nome = req.params.nome;
+  const profilo = getProfileDataAbel(nome);
+  
+  res.render('profilo', profilo);
 });
 
 app.listen(port, () => {
