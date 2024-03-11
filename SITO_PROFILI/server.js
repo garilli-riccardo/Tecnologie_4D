ff5b7db900e836d879a3e2e1d374a365c83f357e
Riccardo Garilli <garilli.riccardo@iisgalvanimi.edu.it>
2024-03-11 20:52:51 +0000

    3

--- SITO_PROFILI/server.js
+++ SITO_PROFILI/server.js
@@ -1,67 +1,27 @@
 const express = require('express');
+const people = require('./people.json'); 
 const app = express();
-const port = 3000;
 
-app.use(express.static('public')); // Per servire file statici come immagini e CSS
-app.set('view engine', 'pug'); // Usa Pug come motore di template
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
 
-function getProfileDataAbel(nome) {
-  return {
-    nome: nome,
-    eta: '30',
-    biografia: 'Una breve biografia del profilo...',
-    genere_musicale: '',
-  };
-}
-
-function getProfileDataCarti(nome) {
-  return {
-    nome: nome,
-    eta: '30',
-    biografia: 'Una breve biografia del profilo...',
-    genere_musicale: '',
-  };
-}
-
-function getProfileDataYe(nome) {
-  return {
-    nome: nome,
-    eta: '30',
-    biografia: 'Una breve biografia del profilo...',
-    genere_musicale: '',
-  };
-}
-
-
-
-// Homepage
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Sito Profili' });
+  res.render('index', { title: 'Homepage' });
 });
 
-// Pagina dei profili
 app.get('/profili', (req, res) => {
-  res.render('profili', { title: 'Profili' });
+  res.render('profili', {
+    title: 'Profili',
+    people: people.profiles 
+  });
 });
-
-// Pagina del profilo della persona
-app.get('/profili/:persona', (req, res) => {
-  const persona = req.params.persona;
-  res.render('persona', { title: `Profilo di ${persona}`, persona });
+app.get('/profile', (req, res) => {
+  const person = people.profiles.find((p) => p.id === req.query.id);
+  res.render('profile', {
+    title: `About ${person.firstname} ${person.lastname}`,
+    person,
+  });
 });
-
-app.get('/profilo/:nome', (req, res) => {
-  const nome = req.params.nome;
-  const profilo = getProfileDataAbel(nome);
-  const profilo2 = getProfileDataYe(nome);
-  const profilo3 = getProfileDataCarti(nome);
- 
-  
-  res.render('profilo', profilo);
-  res.render('profilo', profilo2);
-  res.render('profilo', profilo3);
-});
-
-app.listen(port, () => {
-  console.log(`Server in ascolto su http://localhost:${port}`);
+app.listen(3000, function () {
+ console.log('Example app listening on port 3000!');
 });
